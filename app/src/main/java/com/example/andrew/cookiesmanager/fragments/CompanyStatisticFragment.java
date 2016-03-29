c0f854776900e8570dba47ad4253f3e1722230cc
andrew <andriytheprogrammer@gmail.com>
2016-03-30 00:33:15 +0300

    Chat messages template

---
+++ app/src/main/java/com/example/andrew/cookiesmanager/fragments/CompanyStatisticFragment.java
@@ -0,0 +1,31 @@
+package com.example.andrew.cookiesmanager.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.andrew.cookiesmanager.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CompanyStatisticFragment extends Fragment {
+
+    View rootView;
+
+    public CompanyStatisticFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_company_statistic, container, false);
+        return rootView;
+    }
+
+}
